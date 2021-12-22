b16f7de215f3d72f7dd8d19b7d10de41180d945a
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2021-12-22 10:56:05 +0900

    Add read API

--- pio_usb.h
+++ pio_usb.h
@@ -6,6 +6,9 @@
 
 usb_device_t *pio_usb_init(const pio_usb_configuration_t *c);
 void pio_usb_task(void);
+
+int pio_usb_get_in_data(endpoint_t *ep, uint8_t *buffer, uint8_t len);
 int pio_usb_set_out_data(endpoint_t *ep, const uint8_t *buffer, uint8_t len);
+
 void pio_usb_stop(void);
 void pio_usb_restart(void);
