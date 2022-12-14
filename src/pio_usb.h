babe70a97f8770ba6732e51535d96bcc3eb90dba
hathach <thach@tinyusb.org>
2022-12-14 15:04:56 +0700

    add cpp extern when being included from a cpp file

--- src/pio_usb.h
+++ src/pio_usb.h
@@ -4,6 +4,10 @@
 #include "pio_usb_configuration.h"
 #include "usb_definitions.h"
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 // Host functions
 usb_device_t *pio_usb_host_init(const pio_usb_configuration_t *c);
 int pio_usb_host_add_port(uint8_t pin_dp);
@@ -20,3 +24,7 @@ void pio_usb_device_task(void);
 endpoint_t *pio_usb_get_endpoint(usb_device_t *device, uint8_t idx);
 int pio_usb_get_in_data(endpoint_t *ep, uint8_t *buffer, uint8_t len);
 int pio_usb_set_out_data(endpoint_t *ep, const uint8_t *buffer, uint8_t len);
+
+#ifdef __cplusplus
+ }
+#endif
